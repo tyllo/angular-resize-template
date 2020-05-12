fefeee61c34d14113383a47fae4e209479db91f4
tyllo <gmtyllo@gmail.com>
2020-05-12 22:39:07 +1000

    Fix type diretives -> directives

--- app/modules/viewport-size/viewport-size.module.ts
+++ app/modules/viewport-size/viewport-size.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { IfViewportSizeDirective } from './diretives/if-viewport-size.directive';
+import { IfViewportSizeDirective } from './directives/if-viewport-size.directive';
 import { ViewportSizesService, VIEWPORT_SIZES_SERVICE_SETTINGS } from './providers/viewport-sizes.service';
 
 export { VIEWPORT_SIZES_SERVICE_SETTINGS };
